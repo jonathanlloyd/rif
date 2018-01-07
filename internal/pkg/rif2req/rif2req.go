6fdbd31de7e05c1232e1c59de29c0bf71bbe4935
Jonathan Lloyd <jonathan@thisisjonathan.com>
2018-01-07 21:56:39 +0000

    Add basic rif2req implementation

--- internal/pkg/rif2req/rif2req.go
+++ internal/pkg/rif2req/rif2req.go
@@ -1,15 +1,19 @@
 package rif2req
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 // RifFileV0 is an in-memory representation of the unversioned beta .rif file
 // format
 type RifFileV0 struct {
-	URL    string
-	Method string
+	URL     string
+	Method  string
+	Headers map[string]string
+	Body    *string
 }
 
 var httpMethods = []string{
@@ -26,7 +30,7 @@ var httpMethods = []string{
 
 // Rif2Req takes a parsed .rif file and returns an equivalent stdlib http
 // request struct
-func Rif2Req(rFile RifFileV0) (*http.Request, error) {
+func Rif2Req(rFile RifFileV0, rifVersion string) (*http.Request, error) {
 	// Validate rFile
 	isValidMethod := false
 	for _, method := range httpMethods {
@@ -39,9 +43,24 @@ func Rif2Req(rFile RifFileV0) (*http.Request, error) {
 		return nil, fmt.Errorf("Method %s is invalid", rFile.Method)
 	}
 
-	req, err := http.NewRequest(rFile.Method, rFile.URL, nil)
+	// Create request
+	var body io.Reader
+	if rFile.Body != nil {
+		body = bytes.NewBuffer([]byte(*rFile.Body))
+	}
+	req, err := http.NewRequest(rFile.Method, rFile.URL, body)
 	if err != nil {
 		return nil, err
 	}
+
+	// Add additional headers
+	for headerName, headerValue := range rFile.Headers {
+		req.Header.Add(headerName, headerValue)
+	}
+
+	// Add user agent
+	userAgent := fmt.Sprintf("RIF/%s", rifVersion)
+	req.Header.Add("User-Agent", userAgent)
+
 	return req, nil
 }
--- internal/pkg/rif2req/rif2req_test.go
+++ internal/pkg/rif2req/rif2req_test.go
@@ -2,10 +2,78 @@ package rif2req_test
 
 import (
 	"github.com/turingincomplete/rif/internal/pkg/rif2req"
+	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+// Rif2Req should correctly set req method
+func TestMethodSet(t *testing.T) {
+	rFile := rif2req.RifFileV0{
+		URL:    "http://example.com/test",
+		Method: "GET",
+	}
+	req, _ := rif2req.Rif2Req(rFile, "1.0.0")
+
+	assert.Equal(t, rFile.Method, req.Method)
+}
+
+// Rif2Req should correctly set req URL
+func TestURLSet(t *testing.T) {
+	rFile := rif2req.RifFileV0{
+		URL:    "http://example.com/test",
+		Method: "GET",
+	}
+	req, _ := rif2req.Rif2Req(rFile, "1.0.0")
+
+	assert.Equal(t, "http", req.URL.Scheme)
+	assert.Equal(t, "example.com", req.URL.Host)
+	assert.Equal(t, "/test", req.URL.Path)
+}
+
+// Rif2Req should correctly set user agent
+func TestUserAgentSet(t *testing.T) {
+	rFile := rif2req.RifFileV0{
+		URL:    "http://example.com/test",
+		Method: "GET",
+	}
+	req, _ := rif2req.Rif2Req(rFile, "1.0.0")
+
+	reqUserAgent := strings.Join(req.Header["User-Agent"], "")
+	assert.Equal(t, "RIF/1.0.0", reqUserAgent)
+}
+
+// Rif2Req should correctly set additional req headers
+func TestHeadersSet(t *testing.T) {
+	rFile := rif2req.RifFileV0{
+		URL:    "http://example.com/test",
+		Method: "GET",
+		Headers: map[string]string{
+			"x-fake-header": "value",
+		},
+	}
+	req, _ := rif2req.Rif2Req(rFile, "1.0.0")
+
+	header := strings.Join(req.Header["X-Fake-Header"], "")
+	assert.Equal(t, "value", header)
+}
+
+// Rif2Req should correctly set req body
+func TestBodySet(t *testing.T) {
+	body := "test_body"
+	rFile := rif2req.RifFileV0{
+		URL:    "http://example.com/test",
+		Method: "POST",
+		Body:   &body,
+	}
+	req, _ := rif2req.Rif2Req(rFile, "1.0.0")
+
+	reqBody, err := ioutil.ReadAll(req.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "test_body", string(reqBody))
+}
 
 // Rif2Req should accept valid HTTP methods
 func TestValidMethods(t *testing.T) {
@@ -26,10 +94,9 @@ func TestValidMethods(t *testing.T) {
 			URL:    "http://example.com/test",
 			Method: method,
 		}
-		req, err := rif2req.Rif2Req(rFile)
+		_, err := rif2req.Rif2Req(rFile, "1.0.0")
 
 		assert.Nil(t, err)
-		assert.Equal(t, req.Method, method)
 	}
 }
 
@@ -40,6 +107,6 @@ func TestInvalidMethods(t *testing.T) {
 		Method: "BAD_METHOD",
 	}
 
-	_, err := rif2req.Rif2Req(rFile)
+	_, err := rif2req.Rif2Req(rFile, "1.0.0")
 	assert.NotNil(t, err)
 }
