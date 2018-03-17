1c349819110f850dcd8bd9795fb8804a101234af
Jonathan Lloyd <jonathan@thisisjonathan.com>
2018-03-17 21:15:02 +0000

    Better missing var errors (#2)
    * Add acceptance test
    
    * Add improved error for missing required vars
    
    * Update changelog

--- tests/acceptance-tests/steps/validation.py
+++ tests/acceptance-tests/steps/validation.py
@@ -11,3 +11,23 @@ def step_impl(context):
     context.expected_error_msg = "Error parsing .rif file: rif file version " \
             "greater than maxium supported version - 0"
 
+
+@given(u'a .rif file is on disk that has some required variables')
+def step_impl(context):
+    context.filename = '/vol/tests/test-data/required-variables.rif'
+    context.expected_error_msg = """
+Invalid parameters: 
+Missing required variable(s): VAR_A
+
+The variables for this RIF file are as follows:
+Required:
+ - VAR_A ( number )
+Optional:
+ - VAR_B ( string, default=value )
+"""[1:-1]
+
+@when(u'the user runs RIF on that file without passing in those variables')
+def step_impl(context):
+    context.stdout, context.returncode = run_rif([
+        context.filename,
+    ])
